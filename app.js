1ac6be3292871225ea330bcdfe9eb3c732b42080
Yenkobe <yenduykr@gmail.com>
2022-06-06 16:12:21 +0900

    Campground Model

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/YelpCamp')
+// NOT LONGER NECESSARY 
+//The mongoose package just recently updated to version 6.x after years, 
+//and the useCreateIndex/useNewUrlParser /useUnifiedTopology property is no longer required to dismiss some warnings.
+//      {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+// });
+
+// logic to check if there is an error
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
+
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -9,7 +31,13 @@ app.set('views', path.join(__dirname, 'views'))
 
 
 app.get('/', (req, res) => {
-    res.send('welcome test')
+    res.render('home')
+})
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard', description: 'Cheap Camping' });
+    await camp.save();
+    res.send(camp)
 })
 
 app.listen(4000, () => {
