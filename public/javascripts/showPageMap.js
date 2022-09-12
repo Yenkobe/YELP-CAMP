397f375fa2c7b231964c84cde0d4f215f6b29311
Yenkobe <yenduykr@gmail.com>
2022-09-12 12:14:22 +0900

    Home page styling

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -7,12 +7,15 @@ const map = new mapboxgl.Map({
     projection: 'globe' // display the map as a 3D globe
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
+
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
     .setPopup(
         new mapboxgl.Popup({ offset: 25 })
             .setHTML(
-                `<h3>${campground.title}</h3>`
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
             )
     )
     .addTo(map);
